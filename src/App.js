f81343c51afe5c94418868212e54fd8dbf53096f
KaminskyVova <kamok@bk.ru>
2020-08-01 21:06:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="home">
+        <h1>Ready for WAR</h1>
+        <input type="text" placeholder="enter your name"></input>
+        <button>Start</button>
+      </div>
+      <hr />
+
+      <div className="field">
+        <p>Computer</p>
+        <div>4</div>
+        <div>4</div>
+        <p>you</p>
+        <button>next</button>
+      </div>
+      <hr />
+
+      <div className="result">
+        <span>LOSE\WIN</span>
+        <div>
+          <span>1</span>--<span>0</span>
+        </div>
+        <button>Again?</button>
+      </div>
+      <hr />
+
     </div>
   );
 }
